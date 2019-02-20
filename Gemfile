87933541b2bf46aded26286ec24ef0febf8c67a3
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-02-20 16:43:37 +0000

    DB seeded with 10 Flats

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'simple_form'
 gem 'autoprefixer-rails'
 gem 'jquery-rails'
 
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
+
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating Flats'
+
+10.times {
+  flat = Flat.new(name: Faker::GreekPhilosophers.name, address: Faker::Address.street_address, description: Faker::Restaurant.description, price_per_night: rand(100..1000), number_of_guests: rand(1..10))
+  flat.save
+}
+puts 'Finished!'
